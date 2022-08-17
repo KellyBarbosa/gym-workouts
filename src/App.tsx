79ac41e1c5edde173e1f59f7717ebe69e9681b75
KellyBarbosa <kelly.189@hotmail.com>
2022-08-16 22:25:49 -0300

    Adicionando os tipos nos exercícios

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-interface IExercise extends ITypesExercise {
+interface IExercise {
   name: string;
   series: string;
   repeat: string;
   weight: string;
+  type: typeExercise[];
 }
 
-interface ITypesExercise {
-  type: 'Peito' | 'Ombro' |   'Tríceps' |   'Costas' |   'Panturrilha' |  'Quadríceps' |  'Posterior' |   'Glúteo' | 'Abdominal' | 'Aeróbico' | 'Bíceps'
+enum typeExercise {
+  peito = "Peito",
+  ombro = "Ombro",
+  triceps = "Tríceps",
+  costas = "Costas",
+  panturrilha = "Panturrilha",
+  quadriceps = "Quadríceps",
+  posterior = "Posterior",
+  gluteo = "Glúteo",
+  abdominal = "Abdominal",
+  aerobico = "Aeróbico",
+  biceps = "Bíceps",
 }
 
 function App() {
@@ -20,56 +31,56 @@ function App() {
       series: "4",
       repeat: "Falha",
       weight: "Aumentando a carga",
-      type: "Quadríceps"
+      type: [typeExercise.quadriceps, typeExercise.gluteo],
     },
     {
       name: "Avanço livre",
       series: "3",
       repeat: "Falha",
       weight: "0",
-      type: "Quadríceps"
+      type: [typeExercise.quadriceps],
     },
     {
       name: "Hack machine",
       series: "4",
       repeat: "10 + 5 curtas + 10 agachamentos livres",
       weight: "0",
-      type: "Quadríceps"
+      type: [typeExercise.quadriceps],
     },
     {
       name: "Panturrilha sentada",
       series: "5",
       repeat: "15",
       weight: "0",
-      type: "Panturrilha"
+      type: [typeExercise.panturrilha],
     },
     {
       name: "Puxador frente pronado",
       series: "4",
       repeat: "12",
       weight: "0",
-      type: "Costas"
+      type: [typeExercise.costas],
     },
     {
       name: "Elevação lateral",
       series: "3",
       repeat: "12",
       weight: "0",
-      type: "Ombro"
+      type: [typeExercise.ombro],
     },
     {
       name: "Rosca direta W",
       series: "3",
       repeat: "10",
       weight: "0",
-      type: "Bíceps"
+      type: [typeExercise.biceps],
     },
     {
       name: "Esteira",
       series: "6",
       repeat: "6 tiros de 30 segundos + 1 caminhando",
       weight: "0",
-      type: "Aeróbico"
+      type: [typeExercise.aerobico],
     },
   ]);
 
@@ -78,7 +89,10 @@ function App() {
       {exercises.map((exercise) => (
         <li key={exercise.name}>
           {exercise.name} | {exercise.series} | {exercise.repeat} |{" "}
-          {exercise.weight} - <strong>{exercise.type}</strong> 
+          {exercise.weight} -
+          {exercise.type.map((et) => (
+            <strong> {et}</strong>
+          ))}
         </li>
       ))}
     </div>
